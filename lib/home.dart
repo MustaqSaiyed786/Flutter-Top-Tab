3e896c2cec8564a28eec433fa9ee9428d86eccde
apple <saiyedmscit16@gmail.com>
2019-05-06 16:05:19 +0530

    - init

---
+++ lib/home.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: new Center(
+        child: new Column(
+          children: <Widget>[
+            new Padding(
+              padding: EdgeInsets.all(20.0),
+            ),
+            new Text(
+              "Home",
+              style: new TextStyle(fontSize: 12.4),
+            ),
+            new Padding(
+              padding: EdgeInsets.all(20.0),
+            ),
+            new Icon(Icons.ac_unit, size: 90.0,color: Colors.black,)
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+      
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin{
+  
+  TabController tabController;
+
+  @override
+  void initState() {
+    tabController =new TabController(vsync: this,length: 4);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        bottom: new TabBar(
+      controller: tabController,
+      tabs: <Widget>[
+        new Tab(icon: new Icon(Icons.android),),
+        new Tab(icon: new Icon(Icons.android),),
+        new Tab(icon: new Icon(Icons.android),),
+      ],
+        ),
+      ),
+      body: Center(),
+    );
+  }
+}
